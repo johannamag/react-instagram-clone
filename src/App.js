b9072350e74d3de2d23992efd68821955cce8bfd
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-30 15:43:02 +0100

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Post />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        height="50"
+        src="https://fontmeme.com/images/instagram-new-logo.png"
+        alt="Logo"
+      />
+    </div>
+  );
+}
+
+export default Header;
