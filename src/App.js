8d746297b721c9700e237a79a48d5c3df891c6de
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-31 10:12:12 +0100

    Map Posts UseState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      caption: "Lorem Ipsum ajks hekjlad eklöaskdle djaksdjlke",
+      username: "Username",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/en/f/f1/Picture_This_Album_Cover.jpg",
+    },
+    {
+      caption: "Lorem Ipsum ajks hekjlad eklöaskdle djaksdjlke",
+      username: "Username",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/en/f/f1/Picture_This_Album_Cover.jpg",
+    },
+  ]);
+
   return (
     <div className="app">
       <Header />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -2,22 +2,18 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 
-function Post() {
+function Post({ imageUrl, username, caption }) {
   return (
     <div className="post">
       <div className="post__header">
         <Avatar className="post__avatar" src="/static/images/avatar/1.jpg" />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
 
-      <img
-        className="post__img"
-        src="https://upload.wikimedia.org/wikipedia/en/f/f1/Picture_This_Album_Cover.jpg"
-        alt=""
-      />
+      <img className="post__img" src={imageUrl} alt="" />
       <h4 className="post__text">
-        <strong>Username </strong>
-        caption
+        <strong>{username} </strong>
+        {caption}
       </h4>
     </div>
   );
